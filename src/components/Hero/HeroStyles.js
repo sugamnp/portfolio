1433f023d1467d8ccc4926e7495b5b228df0c9af
Sugam Neupan <sugamneupane227@gmail.com>
2023-10-27 10:40:22 +0800

    automate keyframe generation

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,6 +1,24 @@
 import styled, { keyframes } from "styled-components";
 import { SectionTitle } from "../../styles/GlobalComponents";
 
+const generateWaveKeyframes = () => {
+  let keyframes = "";
+  for (let i = 0; i <= 100; i += 1) {
+    // Amplitude value determines how high/low the wave goes
+    const amplitude = 25; // Change this value to adjust the wave height
+    // Sine function creates the wave motion, amplitude is added here
+    const rotation = 10 + amplitude * Math.sin((i / 100) * Math.PI * 2);
+    keyframes += `
+      ${i}% {
+        transform: rotate(${rotation}deg);
+      }
+    `;
+  }
+  return keyframes;
+};
+
+const wave = keyframes`${generateWaveKeyframes()}`;
+
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -15,46 +33,14 @@ export const LeftSection = styled.div`
     display: flex;
     flex-direction: column;
 
-    margin: 0 auto;
+    margin: 0 auto;a
   }
-`;
-const wave = keyframes`
-0% {
-  transform: rotate(8.0deg);
-}
-5% {
-  transform: rotate(14.0deg);
-}
-10% {
-  transform: rotate(-14.0deg);
-}
-15% {
-  transform: rotate(18.0deg);
-}
-20% {
-  transform: rotate(-8.0deg);
-}
-25% {
-  transform: rotate(18.0deg);
-}
-30% {
-  transform: rotate(-6.0deg);
-}
-35% {
-  transform: rotate(10.0deg);
-}
-40% {
-  transform: rotate(8.0deg);
-}
-100% {
-  transform: rotate(8.0deg);
-}
 `;
 
 export const HandEmoji = styled(SectionTitle)`
   display: inline-block;
   animation: ${wave};
-  animation-duration: 4s;
+  animation-duration: 2s;
   animation-iteration-count: infinite;
   /* Add any other styles you need for the hand emoji */
 `;
